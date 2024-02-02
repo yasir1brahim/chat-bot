91ceabc1f0877e1f1ddf25a888bdc569247d15a0
Muhammad Zohaib <mzohaib.knysys@gmail.com>
2024-02-02 16:46:00 +0500

    UI fixes.

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-$(function() {
-  var INDEX = 0; 
-  $("#chat-submit").click(function(e) {
+$(function () {
+  var INDEX = 0;
+  $("#chat-submit").click(function (e) {
     e.preventDefault();
-    var msg = $("#chat-input").val(); 
-    if(msg.trim() == ''){
+    var msg = $("#chat-input").val();
+    if (msg.trim() == '') {
       return false;
     }
     generate_message(msg, 'self');
     var buttons = [
-        {
-          name: 'Existing User',
-          value: 'existing'
-        },
-        {
-          name: 'New User',
-          value: 'new'
-        }
-      ];
-    setTimeout(function() {      
-      generate_message(msg, 'user');  
+      {
+        name: 'Existing User',
+        value: 'existing'
+      },
+      {
+        name: 'New User',
+        value: 'new'
+      }
+    ];
+    setTimeout(function () {
+      generate_message(msg, 'user');
     }, 1000)
-    
   })
-  
+
   function generate_message(msg, type) {
     INDEX++;
-    var str="";
-    str += "<div id='cm-msg-"+INDEX+"' class=\"chat-msg "+type+"\">";
+    var str = "";
+    str += "<div id='cm-msg-" + INDEX + "' class=\"chat-msg " + type + "\">";
     str += "          <span class=\"msg-avatar\">";
     str += "            <img class=\"up\" src=\"img/btn-thumb-up.png\" alt=\"Avatar\">";
     str += "            <img class=\"down\" src=\"img/btn-thumb-down.png\" alt=\"Avatar\">";
@@ -36,72 +35,82 @@ $(function() {
     str += "          <\/div>";
     str += "        <\/div>";
     $(".chat-logs").append(str);
-    $("#cm-msg-"+INDEX).hide().fadeIn(300);
-    if(type == 'self'){
-     $("#chat-input").val(''); 
-    }    
-    $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight}, 1000);    
-  }  
-  
-  function generate_button_message(msg, buttons){    
-    /* Buttons should be object array 
-      [
-        {
-          name: 'Existing User',
-          value: 'existing'
-        },
-        {
-          name: 'New User',
-          value: 'new'
-        }
-      ]
-    */
+    $("#cm-msg-" + INDEX).hide().fadeIn(300);
+    if (type == 'self') {
+      $("#chat-input").val('');
+    }
+    $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight }, 1000);
+  }
+
+  function generate_button_message(msg, buttons) {
     INDEX++;
-    var btn_obj = buttons.map(function(button) {
-       return  "              <li class=\"button\"><a href=\"javascript:;\" class=\"btn btn-primary chat-btn\" chat-value=\""+button.value+"\">"+button.name+"<\/a><\/li>";
+    var btn_obj = buttons.map(function (button) {
+      return "              <li class=\"button\"><a href=\"javascript:;\" class=\"btn btn-primary chat-btn\" chat-value=\"" + button.value + "\">" + button.name + "<\/a><\/li>";
     }).join('');
     var str = "";
     str += "<div id='cm-msg-" + INDEX + "' class=\"chat-msg user\">";
     str += "          <span class=\"msg-avatar\">";
     str += "            <img class=\"up\" src=\"img/btn-thumb-up.png\" alt=\"Avatar\">";
     str += "            <img class=\"down\" src=\"img/btn-thumb-down.png\" alt=\"Avatar\">";
-    str += "          </span>"; 
+    str += "          </span>";
     str += "          <div class=\"cm-msg-text\">";
     str += msg;
-    str += "          <\/div>";    
+    str += "          <\/div>";
     str += "          <div class=\"cm-msg-button\">";
-    str += "            <ul>";   
+    str += "            <ul>";
     str += btn_obj;
     str += "            <\/ul>";
     str += "          <\/div>";
     str += "        <\/div>";
     $(".chat-logs").append(str);
-    $("#cm-msg-"+INDEX).hide().fadeIn(300);   
-    $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight}, 1000);
+    $("#cm-msg-" + INDEX).hide().fadeIn(300);
+    $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight }, 1000);
     $("#chat-input").attr("disabled", true);
   }
-  
-  $(document).delegate(".chat-btn", "click", function() {
+
+  $(document).delegate(".chat-btn", "click", function () {
     var value = $(this).attr("chat-value");
     var name = $(this).html();
     $("#chat-input").attr("disabled", false);
     generate_message(name, 'self');
   })
-  
-    $(".chat-box").hide(); // Hide chat-box by default
 
-    $("#chat-circle, .chat-box-toggle").click(function() {
-      $(".chat-box").toggle('scale');
-      if (!$("#chat-circle").hasClass('rectangle')) {
-        $("#chat-circle").addClass('rectangle');
-        $(".close").css('display', 'none');
-        $(".icon-chat").css('display', 'block');
-      } else {
-        $("#chat-circle").removeClass('rectangle');
-        $(".close").css('display', 'block');
-        $(".icon-chat").css('display', 'none');
-      }
+  $(".chat-box").hide(); // Hide chat-box by default
+
+  $("#chat-circle, .chat-box-toggle").click(function () {
+    $(".chat-box").toggle('scale');
+
+    if ($("#chat-circle").hasClass('rectangle')) {
+      $("#chat-circle").removeClass('rectangle');
+    }
+
+    // Toggle the display property of .close and .icon-chat
+    $(".close").toggle();
+    $(".icon-chat").toggle();
+
+    checkScroll(); // Check the scroll position immediately
+  });
+
+
+
+  setTimeout(function () {
+    $('#chat-circle').css('opacity', '1');
+  }, 2000);
+
+  setTimeout(function () {
+    // Listen to the scroll event
+    $(window).scroll(function () {
+      checkScroll();
     });
+  }, 2000);
 
-  
-})
+  function checkScroll() {
+    var chatCircle = $('#chat-circle');
+
+    // Check if the user has scrolled
+    if ($(window).scrollTop() > 0) {
+      // If scrolled, remove the 'rectangle' class
+      chatCircle.removeClass('rectangle');
+    }
+  }
+});
